5dcec1232ea12e7742f71f35a536fff35a92f420
mohanjeeva <mohanjeeva@users.noreply.github.com>
2017-06-05 18:05:25 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class hello
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+for(int i=0;i<5;i++)
+System.out.println("HELLO");
+}
+}
