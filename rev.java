b27882dcf808424749fe12de4620fcdeb64a02f9
mohanjeeva <mohanjeeva@users.noreply.github.com>
2017-06-12 13:36:22 +0530

    Create new file

---
+++ rev.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class rev
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String ss=s.nextLine();
+StringBuffer sss=new StringBuffer(ss);
+System.out.println(sss.reverse());
+}
+}
