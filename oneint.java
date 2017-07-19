df8e343bc266c7396b768615f8d6726249a3af33
mohanjeeva <mohanjeeva@users.noreply.github.com>
2017-07-19 12:07:17 +0530

    Create oneint.java

---
+++ oneint.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class oneint
+{
+    public static void main(String args[])
+    {
+        String s="";
+        for(int i=0;i<args.length;i++)
+        {
+            s+=args[i];
+        }
+        for(int i=0;i<args.length;i++)
+        {
+            String ss=s.replace(args[i],"");
+            if(ss.length()==(s.length()-args[i].length()))
+            System.out.print(args[i]);
+        }
+    }
+}
