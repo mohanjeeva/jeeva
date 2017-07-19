8c9f157433a59fc25868fe53cb7e3f22c9cd225f
mohanjeeva <mohanjeeva@users.noreply.github.com>
2017-07-19 10:31:53 +0530

    Create valueindex.java

---
+++ valueindex.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class valueindex
+{
+    public static void main(String args[])
+    {
+        for(int i=0;i<args.length;i++)
+        {
+            int n=Integer.parseInt(args[i]);
+            if(i==n)
+            System.out.println(n);
+        }
+    }
+}
